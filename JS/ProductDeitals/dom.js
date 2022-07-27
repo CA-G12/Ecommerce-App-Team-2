e45c7986e4a53269b5251ee87c494809b9aa4b4e
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-07-27 14:21:18 +0300

    create add to cart function and save product in the local storage related #19

---
+++ JS/ProductDeitals/dom.js
@@ -0,0 +1,11 @@
+let img = document.getElementById("img");
+let title = document.getElementById("title");
+let desc = document.querySelector('.description');
+let addBtn = document.querySelector('.add-btn') ;
+let price = document.querySelector('.price');
+
+
+addBtn.addEventListener('click',()=>{
+    setItemTolocal(); 
+    })
+    
